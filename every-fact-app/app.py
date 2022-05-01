5c09e95b0df03eddd98b50464e668ff6a7c28f69
John Hollingsworth <hollingsfsdf@gmail.com>
2022-05-01 14:57:53 -0600

    env variable case?

--- every-fact-app/app.py
+++ every-fact-app/app.py
@@ -1,5 +1,6 @@
 import os
 import random
+import string
 from pathlib import Path
 
 import openai
@@ -10,13 +11,12 @@ ROOT = Path(__file__).resolve().parents[0]
 
 
 def lambda_handler(event, context):
-    print("Get credentials")
+    # Authenticate with OpenAI and Twitter
     consumer_key = os.getenv("TwitterConsumerKey")
     consumer_secret = os.getenv("TwitterConsumerSecret")
     access_token = os.getenv("TwitterAccessTokenKey")
     access_token_secret = os.getenv("TwitterAccessTokenSecret")
     openai_token = os.getenv("OpenAiToken")
-    print("Authenticate")
     api = twitter.Api(consumer_key,
                       consumer_secret,
                       access_token,
@@ -24,8 +24,7 @@ def lambda_handler(event, context):
                       )
     openai.api_key = openai_token
 
-    print("configure stuff")
-    profanity.set_censor_characters("*")
+    # Configure some stuff
     # Ada usually just spews nonsense, and Da Vinci costs a lot.
     engines = {
         # "text-davinci-001": "Da Vinci",
@@ -42,43 +41,56 @@ def lambda_handler(event, context):
         "Interesting X Fact:",
     ]
 
-    print("Get a random word")
-    # nltk.data.path.append("/tmp")
-    #
-    # nltk.download('brown', download_dir = "/tmp")
-    # nltk.download('webtext', download_dir = "/tmp")
-    # word_list = list(set(brown.words()) | set(webtext.words()))
+    # Get a random word from the wordlist
+    # We could use the nltk package, but it's slower than this pre-randomized list that I exported
     with open("words.txt", "r", encoding='utf-8') as f:
         word_list = f.read().split(",")
     word = random.choice(word_list)
-    print(word)
-    print("get AI response")
-    prompt = random.choice(prompts).replace("X", word.title())
+
+    # Build the prompt
+    prompt = random.choice(prompts).replace("X", string.capwords(word))
+
+    # Pick a random Temperature value
+    # 1% chance to just go wild with maximum temperature
+    if random.random() > 0.99:
+        temperature = 2.0
+        presence_penalty = 2.0
+        frequency_penalty = 2.0
+    else:
+        temperature = random.normalvariate(1, 0.3) # Get some value between 0.5ish and 1.5ish
+        presence_penalty = random.normalvariate(0,0.1)
+        frequency_penalty = random.normalvariate(0,0.1)
+
+    # Get OpenAI's response
+    # TODO - get more than 1 response and pick one that doesn't contain "There is no evidence" and "Weird X Fact: The Weird X..." because they show up way too often
     response = openai.Completion.create(
         engine=random.choice(list(engines.keys())),
         prompt=prompt,
         max_tokens=100,
-        temperature=0.8,
+        temperature=temperature,
         echo=True,
-        stop=". "
+        presence_penalty=frequency_penalty,
+        frequency_penalty=presence_penalty,
+        # stop="."
     )['choices'][0]['text']
 
     # https://en.wikipedia.org/wiki/Scunthorpe_problem
     # Honestly I think its hilarious that it might
     # write a fact about 'Charles ****ens'
+    profanity.set_censor_characters("*")
     response = profanity.censor(response)
 
-    # Lol
-    response = response.strip() \
-        .strip('\n') \
-        .replace("\n", " ") \
-        .replace("\"", "") \
-        .replace("  ", " ") \
-        .replace("  ", " ") \
-        .replace("  ", " ")
+    # Clean up newlines and double spaces. Cut the response to tweet length
+    response = response.replace("\n", " ").replace("  ", " ").strip()
+    response = response[:250]
 
-    print("Post to Twitter")
-    tweet = response[:250]
-    api.PostUpdate(tweet)
+    # if response doesnt end in a period, either cut off the sentence at the last period or add one.
+    if not response.endswith("."):
+        if response.find(".") != -1:
+            response = response[:response.rfind(".")] + "."
+        else:
+            response += '.'
+    print(f"Posting tweet: '{response}'")
+    api.PostUpcdate(response)
 
-    return {"statusCode": 200, "tweet": tweet}
+    return {"statusCode": 200, "tweet": response}
